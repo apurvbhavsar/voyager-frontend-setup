54a76c51a03040cf56d20804fefdd6a347246e65
Apurv Bhavsar <apurv.bhavsar.09@gmail.com>
2023-08-11 13:50:57 +0530

    first commit

---
+++ config/voyager-site.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'views' => 'voyager-site::site',
+
+    'homepage-blade' => 'home',
+
+    'default-blade' => 'default',
+
+    'extra' => [
+        // [
+        //     'model' => '',
+        //     'slug' => '',
+        //     'scope' => [],
+        //     'slug' => '',
+        //     'seo' => [
+        //         'title' => 'column_name',
+        //         'description' => 'column_name',
+        //         'image' => 'column_name',
+        //     ],
+        //     'template' => 'blade_name', // used default if not present or null
+        // ]
+    ]
+];
